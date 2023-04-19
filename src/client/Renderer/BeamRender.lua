84c20061b468d9bfbb9aaedee6408052c4ec88b3
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2023-04-19 02:28:14 -0700

    Started input handling

--- src/client/Renderer/BeamRender.lua
+++ src/client/Renderer/BeamRender.lua
@@ -2,52 +2,55 @@ local BeamRender = {}
 
 BeamRender.inteface = {}
 BeamRender.schema = {}
-BeamRender.metatable = {__index = BeamRender}
+BeamRender.metatable = { __index = BeamRender }
 
 local Point = require(script.Parent.Parent.Physics.Point)
 type Point = Point.Point
 local Beam = require(script.Parent.Parent.Physics.Beam)
 type Beam = Beam.Beam
+local PointRender = require(script.Parent.PointRender)
 
 local BridgeGui = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("BridgeGui")
 
-function BeamRender.interface.new(Point1 : Point, Point2: Point, Thickness: number)
-    local self = {}
+function BeamRender.interface.new(P1: Vector2, P2: Vector2, Thickness: number)
+	local self = {}
 
-    self.Point1 = Point1
-    self.Point2 = Point2
+	self.Point1Render = PointRender.new(P1)
+	self.Point2Render = PointRender.new(P2)
+    self.Point1 = self.Point1Render.Point
+    self.Point2 = self.Point2Render.Point
 
-    self.Beam = Beam.new(Point1, Point2)
+	self.Beam = Beam.new(self.Point1, self.Point2)
 
-    local Element = Instance.new("Frame")
-    local Position = (Point2.Position - Point1.Position) / 2 + Point1.Position
-    local Rotation = math.deg(math.atan2(Point2.Position.Y - Position.X, Point2.Position.X - Position.X))
-    local Size = (Point2.Position - Point1.Position).Magnitude
-    Element.Position = UDim2.fromOffset(Position.X, Position.Y)
-    Element.Orientation = Rotation
-    Element.Size = UDim2.fromOffset(Size, Thickness)
+	local Element = Instance.new("Frame")
+	local Position = (P2.Position - P1.Position) / 2 + P1.Position
+	local Rotation = math.deg(math.atan2(P2.Position.Y - Position.X, P2.Position.X - Position.X))
+	local Size = (P2.Position - P1.Position).Magnitude
+	Element.Position = UDim2.fromOffset(Position.X, Position.Y)
+	Element.Orientation = Rotation
+	Element.Size = UDim2.fromOffset(Size, Thickness)
 
-    local UICorner = Instance.new("UICorner")
-    UICorner.CornerRadius = UDim2.new(1, 0)
-    UICorner.Parent = Element
+	local UICorner = Instance.new("UICorner")
+	UICorner.CornerRadius = UDim2.new(1, 0)
+	UICorner.Parent = Element
 
-    local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
-    UIAspectRatioConstraint.AspectRatio = 1
-    UIAspectRatioConstraint = Element
-    Element.Parent = BridgeGui
+	local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+	UIAspectRatioConstraint.AspectRatio = 1
+	UIAspectRatioConstraint = Element
+	Element.Parent = BridgeGui
 
-    self.Element = Element
+	self.Element = Element
 
-    return setmetatable(self, BeamRender)
+	return setmetatable(self, BeamRender)
 end
 function BeamRender.schema:Update()
-    self.Beam:Update()
-    local Position = (self.Point2.Position - self.Point1.Position) / 2 + self.Point1.Position
-    local Rotation = math.deg(math.atan2(self.Point2.Position.Y - Position.X, self.Point2.Position.X - Position.X))
-    local Size = (self.Point2.Position - self.Point1.Position).Magnitude
-    self.Element.Position = UDim2.fromOffset(Position.X, Position.Y)
-    self.Element.Orientation = Rotation
-    self.Element.Size = UDim2.fromOffset(Size, self.Element.Size.Y.Offset)
+	self.Beam:Update()
+	local Position = (self.Point2.Position - self.Point1.Position) / 2 + self.Point1.Position
+	local Rotation = math.deg(math.atan2(self.Point2.Position.Y - Position.X, self.Point2.Position.X - Position.X))
+	local Size = (self.Point2.Position - self.Point1.Position).Magnitude
+	self.Element.Position = UDim2.fromOffset(Position.X, Position.Y)
+	self.Element.Orientation = Rotation
+	self.Element.Size = UDim2.fromOffset(Size, self.Element.Size.Y.Offset)
 end
 type BeamRender = typeof(BeamRender.interface.new(table.unpack(...)))
-return BeamRender
+return BeamRender.inteface
--- src/client/Renderer/PointRender.lua
+++ src/client/Renderer/PointRender.lua
@@ -2,43 +2,49 @@ local PointRender = {}
 
 PointRender.interface = {}
 PointRender.schema = {}
-PointRender.metatable = {__index = PointRender}
+PointRender.metatable = { __index = PointRender }
 
 local Point = require(script.Parent.Parent.Physics.Point)
 type Point = Point.Point
 
 local BridgeGui = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("BridgeGui")
-function PointRender.interface.new(Position: Vector2, Size : number)
-    local self = {}
 
-    self.Point = Point.new(Position)
+local PointRenderArray = {}
+function PointRender.interface.new(Position: Vector2, Size: number)
+	local self = {}
 
-    local Element = Instance.new("Frame")
-    Element.Position = UDim2.fromOffset(Vector2.X, Vector2.Y)
-    Element.Size = UDim2.fromOffset(Size, Size)
+	self.Point = Point.new(Position)
 
-    local UICorner = Instance.new("UICorner")
-    UICorner.CornerRadius = UDim2.new(1, 0)
-    UICorner.Parent = Element
+	local Element = Instance.new("Frame")
+	Element.Position = UDim2.fromOffset(Vector2.X, Vector2.Y)
+	Element.Size = UDim2.fromOffset(Size, Size)
 
-    local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
-    UIAspectRatioConstraint.AspectRatio = 1
-    UIAspectRatioConstraint = Element
-    Element.Parent = BridgeGui
+	local UICorner = Instance.new("UICorner")
+	UICorner.CornerRadius = UDim2.new(1, 0)
+	UICorner.Parent = Element
 
-    self.Element = Element
+	local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+	UIAspectRatioConstraint.AspectRatio = 1
+	UIAspectRatioConstraint = Element
+	Element.Parent = BridgeGui
 
-    return setmetatable(self, PointRender)
+	self.Element = Element
+    PointRenderArray[Element] = self
+
+	return setmetatable(self, PointRender)
+end
+function PointRender.interface.getFromElement(Element)
+    return PointRenderArray[Element]
 end
 function PointRender.schema:Update()
-    self.Point:Update()
-    self:SetPosition(self.Point.Position)
+	self.Point:Update()
+	self:SetPosition(self.Point.Position)
 end
-function PointRender.schema:SetSize(Size : number)
-    self.Element.Size = UDim2.fromOffset(Size, Size)
+function PointRender.schema:SetSize(Size: number)
+	self.Element.Size = UDim2.fromOffset(Size, Size)
 end
-function PointRender.schema:SetPosition(Position : Vector2)
-    self.Element.Position = UDim2.fromOffset(Vector2.X, Vector2.Y)
+function PointRender.schema:SetPosition(Position: Vector2)
+	self.Element.Position = UDim2.fromOffset(Vector2.X, Vector2.Y)
 end
 type PointRender = typeof(PointRender.interface.new(table.unpack(...)))
-return PointRender
+return PointRender.interface
